c77144db2bb5e8ccbfb2131a7745c586d34772ab
Nelson Ortiz <n.ortizf@uniandes.edu.co>
2026-08-16 11:37:39 -0500

    Usando Single Responsability Principle

---
+++ Chapter2/BankApp/src/main/java/org/example/BankStatementCSVParser.java
@@ -0,0 +1,30 @@
+package org.example;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+
+public class BankStatementCSVParser {
+
+    private static final DateTimeFormatter DATEPATTERN
+            = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+
+    private BankTransaction parseFromCSV(final String line){
+        final String [] columns = line.split(",");
+        final LocalDate date = LocalDate.parse(columns[0], DATEPATTERN);
+        final double amount = Double.parseDouble(columns[1]);
+        final String description = columns[2];
+
+        return  new BankTransaction(date, amount, description);
+    };
+
+    public List<BankTransaction> parseLinesFromCSV(final List<String> lines){
+        final List<BankTransaction> bankTransactions = new ArrayList<>();
+        for(final String line: lines){
+            bankTransactions.add(parseFromCSV(line));
+        }
+        return bankTransactions;
+    }
+
+}
--- Chapter2/BankApp/src/main/java/org/example/BankTransactionAnalyzerSimple.java
+++ Chapter2/BankApp/src/main/java/org/example/BankTransactionAnalyzerSimple.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.time.Month;
+import java.util.ArrayList;
 import java.util.List;
 
 public class BankTransactionAnalyzerSimple {
@@ -11,15 +13,42 @@ public class BankTransactionAnalyzerSimple {
     private static final String RESOURCES = "BankApp/src/main/resources/";
 
     public static void main(String[] args) throws IOException {
-        final Path path = Paths.get(RESOURCES + args[0]);
+
+        final BankStatementCSVParser bankStatementCSVParser = new BankStatementCSVParser();
+
+        final String fileName = args[0];
+        final Path path = Paths.get(RESOURCES + fileName);
         final List<String> lines = Files.readAllLines(path);
+
+        final List<BankTransaction> bankTransactions
+                = bankStatementCSVParser.parseLinesFromCSV(lines);
+
+        System.out.println("The total for all transactions is " +
+                calculateTotalAmount(bankTransactions));
+
+        System.out.println("Transactions in January " +
+                selectInMonth(bankTransactions, Month.JANUARY));
+
+    }
+
+    public static double calculateTotalAmount(final List<BankTransaction> bankTransactions){
         double total = 0d;
-        for(final String line: lines){
-            final String[] columns = line.split(",");
-            final double amount = Double.parseDouble(columns[1]);
-            total += amount;
+        for(BankTransaction bankTransaction: bankTransactions){
+            total += bankTransaction.getAmount();
         }
-        System.out.println("The total for all transactions is "+ total);
+        return  total;
+    }
+
+    public static List<BankTransaction> selectInMonth(final List<BankTransaction> bankTransactions,
+                                                      final Month month){
+
+        final List<BankTransaction> bankTransactionsInMonth = new ArrayList<>();
+        for(final BankTransaction bankTransaction: bankTransactions){
+            if(bankTransaction.getDate().getMonth()==month){
+                bankTransactionsInMonth.add(bankTransaction);
+            }
+        }
+        return bankTransactionsInMonth;
     }
 
 }
