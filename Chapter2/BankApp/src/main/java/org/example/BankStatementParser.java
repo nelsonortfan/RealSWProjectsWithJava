4c3eefc57057a77f62a5bc2849f49b6be06dea14
Nelson Ortiz <n.ortizf@uniandes.edu.co>
2026-08-16 16:06:55 -0500

    desacoplamiento usando interfaz

---
+++ Chapter2/BankApp/src/main/java/org/example/BankStatementParser.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import java.util.List;
+
+public interface BankStatementParser {
+
+    BankTransaction parseFrom(String line);
+    List<BankTransaction> parseLinesFrom(List<String> lines);
+
+}
--- Chapter2/BankApp/src/main/java/org/example/BankTransactionAnalyzerSimple.java
+++ Chapter2/BankApp/src/main/java/org/example/BankTransactionAnalyzerSimple.java
@@ -1,48 +1,16 @@
 package org.example;
 
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.time.Month;
-import java.util.ArrayList;
-import java.util.List;
 
 public class BankTransactionAnalyzerSimple {
 
-    private static final String RESOURCES = "BankApp/src/main/resources/";
-
     public static void main(String[] args) throws IOException {
 
-        final BankStatementCSVParser bankStatementCSVParser = new BankStatementCSVParser();
+        final BankStatementParser bankStatementParser = new BankStatementCSVParser();
 
-        final String fileName = args[0];
-        final Path path = Paths.get(RESOURCES + fileName);
-        final List<String> lines = Files.readAllLines(path);
+        final BankStatementAnalyzer bankStatementAnalyzer = new BankStatementAnalyzer();
 
-        final List<BankTransaction> bankTransactions
-                = bankStatementCSVParser.parseLinesFromCSV(lines);
-
-        final BankStatementProcessor bankStatementProcessor =
-                new BankStatementProcessor(bankTransactions);
-
-        collectSummary(bankStatementProcessor);
-    }
-
-
-    private static void collectSummary(final BankStatementProcessor bankStatementProcessor){
-
-        System.out.println("The total transactions for all transactions is "
-        + bankStatementProcessor.calculateTotalAmount());
-
-        System.out.println("The total transactions in January is "
-        + bankStatementProcessor.calculateTotalInMonth(Month.JANUARY));
-
-        System.out.println("The total transactions in February is "
-        + bankStatementProcessor.calculateTotalInMonth(Month.FEBRUARY));
-
-        System.out.println("The total salary received is " +
-                bankStatementProcessor.calculateTotalForCategory("Salary"));
+        bankStatementAnalyzer.analyze(args[0], bankStatementParser);
 
     }
 
